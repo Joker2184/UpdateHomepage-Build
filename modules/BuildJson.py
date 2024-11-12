32681cf40d63d178ad4acbd511b66ea7b8d3797e
MR.JOKER <ltdove_cn@outlook.com>
2024-11-12 18:17:55 +0800

    Update BuildJson.py

--- modules/BuildJson.py
+++ modules/BuildJson.py
@@ -4,7 +4,8 @@ from homepagebuilder.interfaces.Events import on
 @on('page.generate.return')
 def your_func_name(page, env, result):
     try:
-        subprocess.run(['python', r'H:\Project\modules\VersionJson.py'], check=True)
+        # 修改为在 B 库文件夹的 Modules 文件夹下运行 VersionJson.py
+        subprocess.run(['python', r'file-repo/Modules/VersionJson.py'], check=True)
         print("VersionJson已构建")
     except subprocess.CalledProcessError as e:
         print(f"构建 VersionJson 时发生错误: {e}")
