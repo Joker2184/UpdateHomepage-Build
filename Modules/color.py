5bbfa857581460466b37380a22950020b4de1517
MR.JOKER <ltdove_cn@outlook.com>
2024-11-13 17:47:36 +0800

    Delete Modules/color.py

--- Modules/color.py
+++
@@ -1,9 +0,0 @@
-from homepagebuilder.interfaces import script, require
-
-parsers_module = require('markdown_parsers')
-handles = parsers_module.handles
-InlineNode = parsers_module.InlineNode
-@handles('color')
-class Color(InlineNode):
-    def get_replacement(self):
-        return {'color': self.attrs['color']}
