bc4c6928074b0dc531afefd04aaf56624e4008ac
MR.JOKER <ltdove_cn@outlook.com>
2024-11-16 15:54:20 +0800

    Update GetPRList.py

--- Actions/GetPRList.py
+++ Actions/GetPRList.py
@@ -113,8 +113,8 @@ if not pr_data:
     print("未获取到任何 PR 数据，可能是当前没有打开的 Pull Requests。")
 else:
     # 直接保存到根目录下
-    output_json = " ./UpdateHomepage-Build/PRDatabase.json"  # 不用 os.path.join 直接指定文件名
-    output_xaml = "./UpdateHomepage-Build/libraries/Homepage/PRList.xaml"  # 不用 os.path.join 直接指定路径
+    output_json = " ./PRDatabase.json"  # 不用 os.path.join 直接指定文件名
+    output_xaml = "./libraries/Homepage/PRList.xaml"  # 不用 os.path.join 直接指定路径
     
     save_to_json(pr_data, output_json)
     save_to_xaml(generate_template(pr_data[0]), output_xaml)
