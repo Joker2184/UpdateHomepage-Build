4857184e3470ffbf76bc772e5274ffa15df06a56
Nattiden <2682645990@qq.com>
2024-11-28 22:41:40 +0800

    auto update

---
+++ Actions/update.sh
@@ -0,0 +1,20 @@
+version=$(gh api \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    repos/Hex-Dragon/PCL2/releases \ 
+    | grep tag_name | head -n 1 | cut -d '"' -f 4)
+
+file_path="libraries/$version.md"
+if [ -e "$file_path" ]; then
+    echo "文件存在，无需更改"
+else
+    echo "文件不存在，正在更新"
+    echo "---\nnew: \"true\"\n---\n" > $file_path
+fi
+
+sed -i '' "2s/.*/- \"${version}\"/" pages/UpdateHomepage.yml
+
+git config --local user.email "github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git add *
+git diff-index --quiet HEAD || git commit -m "Update to $version" && git push
