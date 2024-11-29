f5e9e3efce77421fc3951979d74608d15a1be8f1
零棱_ZeroPrism@零维棱镜企划 <ltdove_cn@outlook.com>
2024-11-29 15:37:41 +0800

    Update CheckPRList.sh

--- Actions/CheckPRList.sh
+++ Actions/CheckPRList.sh
@@ -6,6 +6,15 @@ title=$(gh api -H "Accept: application/vnd.github+json" \
 
 # 基于 PR 编号创建文件路径
 file_path="libraries/Homepage/PR#$number.xaml"
+previous_file_path="libraries/Homepage/PR#$((number - 1)).xaml"
+
+# 删除旧的 PR 文件（如果存在）
+if [ -e "$previous_file_path" ]; then
+    echo "删除旧的 XAML 文件: $previous_file_path"
+    rm "$previous_file_path"
+else
+    echo "没有找到旧的 XAML 文件，继续更新"
+fi
 
 # 判断是否存在该文件
 if [ -e "$file_path" ]; then
@@ -55,6 +64,7 @@ else
     </StackPanel>
 </local:MyCard>
 EOF
+    # 更新 pages/UpdateHomepage.yml 的第 6 行
     sed -i "6s/.*/- PR#$number/" pages/UpdateHomepage.yml
 fi
 
