c48b1824dd7f23d8093be0f447f2a92be929f241
MR.JOKER <ltdove_cn@outlook.com>
2024-12-09 17:49:15 +0800

    back

--- Actions/CheckPCLVersion.sh
+++ Actions/CheckPCLVersion.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # 获取版本号 (tag_name)
 version=$(gh api \
     -H "Accept: application/vnd.github+json" \
@@ -19,12 +17,12 @@ created_at=$(gh api \
     repos/Hex-Dragon/PCL2/releases | grep -o '"created_at": *"[^"]*"' | head -n 1 | cut -d '"' -f 4 | cut -d 'T' -f 1)
 
 # 根据发布类型选择链接
-if [ "$release_type" = "true" ]; then
+if [ "$release_type" == "true" ]; then
     # 如果是 pre-release，使用指定的链接
-    link="https://pic.imgdb.cn/item/66fde7a30a206445e36ebafe.png"
+    link="https://pic.imgdb.cn/item/66fde7a30a206445e36ebb11.png"
 else
     # 如果是正式版本，使用 I_Link 链接
-    link="https://pic.imgdb.cn/item/66fde7a30a206445e36ebb11.png"
+    link="https://pic.imgdb.cn/item/66fde7a30a206445e36ebafe.png"
 fi
 
 # 文件路径
@@ -36,20 +34,13 @@ if [ -e "$file_path" ]; then
 else
     echo "文件不存在，正在更新"
     # 创建或更新文件内容，确保格式正确
-    echo -e "---\nnew: \"true\"\ndate: $created_at\nI_Link: $link\nWriter: Null\n---\n" > "$file_path"
+    echo "---\nnew: \"true\"\ndate: $created_at\nI_Link: $link\nWriter: Null\n---\n" > "$file_path"
     # 更新页面配置
     sed -i "2s/.*/- \"${version}\"/" pages/UpdateHomepage.yml
-
-    # 使用 GitHub API 触发 repository_dispatch 事件
-curl -X POST \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token $PAT_TOKEN" \
-    https://api.github.com/repos/Joker2184/HomepageBuilder/dispatches \
-    -d '{"event_type": "trigger-a-build"}'
 fi
 
 # 配置 Git 用户信息并提交更改
 git config --local user.email "Joker2184@outlook.com"
 git config --local user.name "Joker2184Bot"
 git add *
-git diff-index --quiet HEAD || (git commit -m "Update PCL to ${version}" && git push)
+git diff-index --quiet HEAD || (git commit -m "Update PCL version file" && git push)
