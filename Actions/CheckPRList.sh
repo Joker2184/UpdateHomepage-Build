58914cb22192916046a5dd6055a18f0634bc5644
零棱_ZeroPrism <ltdove_cn@outlook.com>
2025-04-04 16:50:59 +0800

    Update CheckPRList.sh

--- Actions/CheckPRList.sh
+++ Actions/CheckPRList.sh
@@ -62,7 +62,7 @@ else
 </local:MyCard>
 EOF
     # 更新 pages/UpdateHomepage.yml 的第 3 行
-    sed -i "3s/.*/- PR#$number/" pages/UpdateHomepage.yml
+    sed -i "4s/.*/- PR#$number/" pages/UpdateHomepage.yml
 
     # 设置文件更新标志
     file_updated=true
